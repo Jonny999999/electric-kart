95e88d1b3c8cce12d496f283187780a08ff8918e
jonny_l480 <jonny@wwad.de>
2024-08-27 20:55:32 +0200

    PWM-Freq tests log, logging, basic IO-test - success
    Board installed and successfully tested
    All practical pwm frequencies were tested
    Basic IO pass through (buzzer at low voltage, fan at switch...)

---
+++ firmware/V2.1/README.md
@@ -0,0 +1,2 @@
+Firmware for control board version V2.1 (`hardware/V2.1_board-ecar_isolated`).  
+This firmware project is started from scratch and is no longer based on the code from V1.
--- firmware/V2.1/main.c
+++ firmware/V2.1/main.c
@@ -8,29 +8,29 @@
 //==== PWM-config ====
 //====================
 // Maximum count value for Fast PWM 
-#define PWM_RESOLUTION 1023  //Possible values: 1023(10Bit), 511(9Bit), 255(8Bit)
+#define PWM_RESOLUTION 1023 //Possible values: 1023(10Bit), 511(9Bit), 255(8Bit)
 
 // Factor clock is reduced before counting up the value
-#define PWM_PRESCALER 64 // Possible values: 1, 8, 64, 256, 1024
+#define PWM_PRESCALER 1 // Possible values: 1, 8, 64, 256, 1024
 
 //Pre-calculated possible combinations:
 /*
-Resolution,	Prescaler, PWM-Freq @ 8Mhz
-1023	  1	      7813
-1023	  8	      977
-1023	  64	    122  <-- used in V1
+|  Resolution  |  Prescaler  |  PWM-Freq @ 8MHz  |  Comment  |
+1023	  1	      7813      (2) >> OK - minor beeping, temperature just fine with fan on
+1023	  8	      977       (5) >> still beeping, a little less annoying than 2kHz (4)
+1023	  64	    122       (8) <== used in V1 ==> loud humming and metal vibrations
 1023	  256	    31
 1023	  1024	  8
 		
-511	    1	      15625
-511	    8	      1953
-511	    64	    244
+511	    1	      15625     (1) >> OK - no sound at all, but IGBT gets VERY-HOT in seconds
+511	    8	      1953      (4) >> annoying beeping, similar to 3.9kHz (3)
+511	    64	    244       (7) >> OK - quieter than V1.0 122Hz (8)
 511	    256	    61
 511	    1024	  15
 		
 255	    1	      31250
-255	    8	      3906
-255	    64	    488
+255	    8	      3906      (3) >> very annoying/loud beeping
+255	    64	    488       (6) >> annoying low beep
 255	    256	    122
 255	    1024  	31
 */
@@ -47,17 +47,17 @@ Resolution,	Prescaler, PWM-Freq @ 8Mhz
 //====================
 //====== config ======
 //====================
-#define GAS_PEDAL_MIN 100 // actual 172
-#define GAS_PEDAL_MAX 900
+#define GAS_PEDAL_MIN 90 // actual 82
+#define GAS_PEDAL_MAX 542 //actual 543
 
 #define FAN_TURN_ON_DUTY 50
 #define FAN_TURN_OFF_DELAY_MS 3000
 
 
 
-//===========================
-//===== Assign GPO Pins =====
-//===========================
+//===============================
+//===== Configure GPIO Pins =====
+//===============================
 //outputs
 GPIO_Pin relay = { PD3, &PORTD, &DDRD, &PIND };
 GPIO_Pin pwm = {PB1, &PORTB, &DDRB, &PINB};
@@ -67,12 +67,13 @@ GPIO_Pin ledExternal = {PB2, &PORTB, &DDRB, &PINB};
 GPIO_Pin buzzer = {PC5, &PORTC, &DDRC, &PINC};
 
 //inputs
-GPIO_Pin switch1 = {PC2, &PORTC, &DDRC, &PINC};
-GPIO_Pin switch2 = {PC3, &PORTC, &DDRC, &PIND};
+GPIO_Pin switch1 = {PC3, &PORTC, &DDRC, &PINC};
+GPIO_Pin switch2 = {PC2, &PORTC, &DDRC, &PINC};
 GPIO_Pin analog = {PC0, &PORTC, &DDRC, &PINC};
 GPIO_Pin gasPedal = {PC1, &PORTC, &DDRC, &PINC};
 GPIO_Pin batteryThreshold = {PD2, &PORTD, &DDRD, &PIND};
 
+//Initialize Pins (define direction)
 void init_gpios(){
   //outputs
   GPIO_Init(&relay, 1);
@@ -103,7 +104,7 @@ void Timer1_FastPWM_Init(void)
   TCCR1A = (1 << WGM10) | (1 << WGM11); // Mode 7: Fast PWM, 10-bit
   #elif PWM_RESOLUTION == 511
   TCCR1A = (0 << WGM10) | (1 << WGM11); // Mode 6: Fast PWM, 9-bit
-  #elif PWM_RESOLUTION == 254
+  #elif PWM_RESOLUTION == 255
   TCCR1A = (1 << WGM10) | (0 << WGM11); // Mode 5: Fast PWM, 8-bit
   #else
     #error "invalid PWM_RESOLUTION value"
@@ -145,48 +146,71 @@ void enable_pwm(){
 }
 
 
+//max duty is configured PWM_RESOLUTION
+//note: due to fast-pwm mode pin can never be fully off or on
+// there will still be one off/on cycle at 1023/0 duty
 void Set_PWM_Duty_Cycle(uint16_t duty_cycle)
 {
-  OCR1A = duty_cycle; // Set duty cycle for OC1A
-                      // If using OCR1B, set duty cycle for OC1B
-                      // OCR1B = duty_cycle;
+  //clip to max value
+  if (duty_cycle > PWM_RESOLUTION)
+    duty_cycle = PWM_RESOLUTION;
+
+ // Set duty cycle for OC1A
+  OCR1A = duty_cycle;
+  // If using OCR1B, set duty cycle for OC1B
+  // OCR1B = duty_cycle;
 }
 
 
+typedef enum {FULL_OFF = 0, FULL_ON, PWM} motorState_t;
+const char* motorState_str[] = {"FULL_OFF", "FULL_ON", "PWM"};
 
 
-
+//======================
+//======== main ========
+//======================
 int main(void)
 {
   // init PWM
   Timer1_FastPWM_Init(); // init timer1 in fast PWM mode with 10bit resolution on pin PB1
+  disable_pwm(); // initially off
 
   // init adc
   ADC_Init();
 
   // init gpio
   init_gpios();
+  GPIO_Set(&fan); //turn off fan initially
 
   // beep at startup
   for (int i = 0; i < 3; i++)
   {
     GPIO_Set(&buzzer);
+    GPIO_Set(&ledExternal);
     _delay_ms(60);
     GPIO_Clear(&buzzer);
+    GPIO_Clear(&ledExternal);
     _delay_ms(100);
   }
+  // external led always on
+  GPIO_Set(&ledExternal);
+
+
   // init UART
   uart_init();
   uart_sendStr("startup finished\n");
-  printf("CPU Frequency is %d Hz\n", F_CPU);
+  printf("CPU Frequency is %ld Hz\n", F_CPU);
   printf("PWM Frequency is %d Hz\n", PWM_FREQUENCY);
 
 
   // variables
-  uint16_t gasPedalAdc;
-
+  uint32_t gasPedalAdc;
   uint8_t pwmEnabled = 0;
+  motorState_t state = FULL_OFF;
   uint16_t duty = 0;
+  uint32_t time = 0;
+  uint32_t timestamp_turnedOn = 0;
+  uint32_t timestamp_turnedOff = 0;
 
   //==== loop ====
   while (1)
@@ -195,20 +219,23 @@ int main(void)
     // read gas pedal
     gasPedalAdc = ADC_ReadChannel(gasPedal.bit);
 
-    // handle PWM pin
+    //=== handle PWM pin ===
     if (gasPedalAdc <= GAS_PEDAL_MIN) // pedal released
     {
+      state = FULL_OFF;
       // turn off pwm for complete stop
       if (pwmEnabled)
       { 
         disable_pwm();
         pwmEnabled = 0;
+        timestamp_turnedOff = time;
       }
       // set pin constantly low = full off
       GPIO_Clear(&pwm);
     }
     else if (gasPedalAdc >= GAS_PEDAL_MAX) // pedal fully pressed
     {
+      state = FULL_ON;
       // turn off pwm for full on
       if (pwmEnabled)
       {
@@ -220,40 +247,69 @@ int main(void)
     }
     else // pedal anywhere between min/max
     {
+      state = PWM;
       if (!pwmEnabled)
       { // re-enable pwm if previously off
         enable_pwm();
         pwmEnabled = 1;
+        timestamp_turnedOn = time;
       }
       // define duty
       // scale pedal input to pwm value
       duty = (gasPedalAdc - GAS_PEDAL_MIN) * PWM_RESOLUTION / (GAS_PEDAL_MAX - GAS_PEDAL_MIN);
 
 
-      // debug output via UART
-      printf("adc=%04d,  pwm=%04d/%d", gasPedalAdc, duty, PWM_RESOLUTION);
       // update duty
       Set_PWM_Duty_Cycle(duty);
     }
 
 
-    // onboard LED
+
+    //=== logging ===
+    // debug output via UART
+    //note: for some reason wrong values are printed when printing multiple variables in on printf?!
+    printf("adc=%04d", gasPedalAdc); 
+
+    printf(" pedal=%04d/1000",
+      (int32_t)(gasPedalAdc - GAS_PEDAL_MIN) * 1000 / (GAS_PEDAL_MAX - GAS_PEDAL_MIN));
+
+    printf(" pwm=%04d/%d", duty, PWM_RESOLUTION);
+    
+    printf(" state='%s'\n", motorState_str[(int)state]);
+
+
+    //=== onboard LED ===
     // on when motor is running
     GPIO_SetLevel(&ledOnboard, (gasPedalAdc >= GAS_PEDAL_MIN));
 
 
-    // Relay
-    // on when switch1 is high
+
+    //=== fan ===
+    // note: output is inverted
+    // turn fan on when above threshold
+   // if (duty > FAN_TURN_ON_DUTY)
+   //   GPIO_Clear(&fan);
+   // // fan still on, motor off long enough
+   // else if (time - timestamp_turnedOff) > FAN_TURN_OFF_DELAY_MS )
+   //   GPIO_Set(&fan);
+
+    // manual control via switch near emergency stop
+    GPIO_SetLevel(&fan, !(GPIO_Read(&switch2)));
+
+
+    //=== Relay ===
+    // on when switch1 is high (currently not connected)
     GPIO_SetLevel(&relay, GPIO_Read(&switch1));
 
+
+
+    //=== Voltage-Threshold ===
     //beep when battery below voltage threshold (opamp+potentiometer)
     //pass through batt threshold signal to buzzer
-    if (GPIO_Read(&batteryThreshold))
-      GPIO_Clear(&buzzer);
-    else
-      GPIO_Set(&buzzer);
+    GPIO_SetLevel(&buzzer, !(GPIO_Read(&batteryThreshold)));
 
 
     _delay_ms(10);
+    time+=10; //TODO: overflows in 49 days
   }
 }
